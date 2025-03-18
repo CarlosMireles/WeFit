153226c48a2f48f53e4d092aaca8ec4bd5b90927
Eduardo <edaniel2004gainza@gmail.com>
2025-03-18 11:00:52 +0000

    Database configuration

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideStorage, getStorage } from '@angular/fire/storage';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDxjryGnYkp6In-t_kfomLO-wq_hty76q0",
-  authDomain: "wefit-5a03f.firebaseapp.com",
-  projectId: "wefit-5a03f",
-  storageBucket: "wefit-5a03f.appspot.com",
-  messagingSenderId: "825832331908",
-  appId: "1:825832331908:web:6b0e8f7696f908d11400d1",
-  measurementId: "G-J12Y075NCD"
-};
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
-    provideRouter(routes)
-  ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
