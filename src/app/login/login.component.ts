7aab91ecae12fe47d648b1bb3ab6890efb314d31
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-03-21 12:19:56 +0000

    Recuperación de datos del formulario de login y register añadido

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    imports: [
+        FormsModule
+    ],
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  // Aquí se pueden manejar eventos como el envío del formulario en el futuro
+  user = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit(form: any) {
+    this.user.email = form.value.email;
+    this.user.password = form.value.password;
+    console.log('User registered:', this.user);
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  user = {
+    username: '',
+    email: '',
+    password: ''
+  };
 
+  onSubmit(form: any) {
+    this.user.username = form.value.user;
+    this.user.email = form.value.email;
+    this.user.password = form.value.password;
+    console.log('User registered:', this.user);
+  }
 }
