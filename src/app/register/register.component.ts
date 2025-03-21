8bf5fac7fd4e2d38fe16aeeba7c8baca02cdae51
CarlosMireles <carlosmirelesrodriguez1@gmail.com>
2025-03-21 16:21:54 +0000

    Integracion del componente register con el back

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,12 @@
 import { Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { UserService } from '../services/user.service';
+
 
 @Component({
   selector: 'app-register',
   imports: [
-    FormsModule
+    FormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
@@ -13,13 +15,28 @@ export class RegisterComponent {
   user = {
     username: '',
     email: '',
-    password: ''
+    password: '',
+    successMessage: '',
+    errorMessage: ''
   };
 
-  onSubmit(form: any) {
+  constructor(private userService: UserService) {}
+
+  async onSubmit(form: any) {
+    // event.preventDefault();
     this.user.username = form.value.user;
     this.user.email = form.value.email;
     this.user.password = form.value.password;
     console.log('User registered:', this.user);
+    try {
+      const user = await this.userService.registerUser(this.user.email, this.user.password, this.user.username);
+      this.user.successMessage = "Registro exitoso ✅";
+      this.user.errorMessage = '';
+      console.log("Usuario registrado:", user);
+    } catch (error) {
+      this.user.successMessage = '';
+      this.user.errorMessage = "Error al registrar el usuario ❌";
+      console.error("Error en el registro:", error);
+    }
   }
 }
