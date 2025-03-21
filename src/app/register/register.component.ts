c9d757910e513d8eb1c8314f0e50c964a3e8d487
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-03-21 10:39:19 +0000

    añadido login y register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,39 +1,11 @@
 import { Component } from '@angular/core';
-import { UserService } from '../services/user.service';
-import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-register',
+  imports: [],
   templateUrl: './register.component.html',
-  imports: [
-    FormsModule,
-    NgIf
-  ],
-  styleUrls: ['./register.component.css']
+  styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  email: string = '';
-  password: string = '';
-  username: string = '';
-  errorMessage: string = '';
-  successMessage: string = '';
 
-  constructor(private userService: UserService) {}
-
-  async onSubmit(event: Event) {
-    event.preventDefault(); // Evita recargar la página
-
-    try {
-      const user = await this.userService.registerUser(this.email, this.password, this.username);
-      this.successMessage = "Registro exitoso ✅";
-      this.errorMessage = '';
-      console.log("Usuario registrado:", user);
-    } catch (error) {
-      this.successMessage = '';
-      this.errorMessage = "Error al registrar el usuario ❌";
-      console.error("Error en el registro:", error);
-    }
-  }
 }
-
