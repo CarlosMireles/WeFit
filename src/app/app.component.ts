87384276e156795ad7ee5563bf5733ff868cd80a
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-03-25 10:10:15 +0000

    añadido pagina de olvidar contraseña

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, ForgotPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
