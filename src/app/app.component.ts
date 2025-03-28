50aec1f42005d776f8509d0d42d92f5c824a9eb9
OmarOrtS <147737596+OmarOrtS@users.noreply.github.com>
2025-03-28 15:36:02 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
 import {ForgotPasswordFinishComponent} from './forgot-password-finish/forgot-password-finish.component';
+import {ModifyPasswordComponent} from './modify-password/modify-password.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, ForgotPasswordComponent, ForgotPasswordFinishComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, ForgotPasswordComponent, ForgotPasswordFinishComponent, ModifyPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
