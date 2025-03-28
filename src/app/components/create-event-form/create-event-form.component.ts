25080727d4fefe151c557fb2749872868b5f097b
CarlosMireles <carlosmirelesrodriguez1@gmail.com>
2025-03-28 15:14:38 +0000

    crear evento

---
+++ src/app/components/create-event-form/create-event-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-event-form',
+  imports: [],
+  templateUrl: './create-event-form.component.html',
+  styleUrl: './create-event-form.component.css'
+})
+export class CreateEventFormComponent {
+
+}
