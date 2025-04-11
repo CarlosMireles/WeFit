aad985a19bf811888e22a3f01e06954087483b2f
Omar <omar.ortega102@alu.ulpgc.es>
2025-04-11 16:55:51 +0100

    tooltips added to searchbar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
+import {LanguageService} from '../../services/translate.service';
 
 @Component({
     selector: 'app-search-bar',
-    imports: [
-        NgOptimizedImage,
-        RouterLink
-    ],
+  imports: [
+    NgOptimizedImage,
+    RouterLink,
+    TranslatePipe
+  ],
     templateUrl: './search-bar.component.html',
     standalone: true,
     styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
-
+  constructor(private langService: LanguageService) {
+  }
 }
