0ff9a7c3d90ed666673a2156aba8024a33fd1e43
eduardogainzakoller <edaniel2004gainza@gmail.com>
2025-04-25 15:54:53 +0100

    Moddify eventes bug fixed and refactor the css of the filters card

---
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent {
+  @Input() message = '¿Estás seguro?';
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  onConfirm() {
+    this.confirm.emit();
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
+}
