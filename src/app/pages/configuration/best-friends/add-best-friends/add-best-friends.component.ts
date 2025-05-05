c64d74c99a253d4a1a44463a870dbedcd766712f
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-05-05 14:47:54 +0100

    solucion de error usuario no encontrado en mejos a añadir y mensaje de confirmación en añadir mejos

--- src/app/pages/configuration/best-friends/add-best-friends/add-best-friends.component.ts
+++ src/app/pages/configuration/best-friends/add-best-friends/add-best-friends.component.ts
@@ -2,6 +2,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {UserService} from '../../../../services/user.service';
 import {UserTemplateImageNameComponent} from '../user-template-image-name/user-template-image-name.component';
 import {NgForOf, NgIf, NgClass} from '@angular/common';
+import {
+  ConfirmationQuestionComponent
+} from '../../../../components/confirmation-question/confirmation-question.component';
 
 interface UserInfo {
   uid: string;
@@ -16,13 +19,16 @@ interface UserInfo {
     UserTemplateImageNameComponent,
     NgIf,
     NgForOf,
-    NgClass
+    NgClass,
+    ConfirmationQuestionComponent
   ],
   templateUrl: './add-best-friends.component.html',
   styleUrl: './add-best-friends.component.css'
 })
 export class AddBestFriendsComponent implements OnInit{
   @Output() backClickedAddFriend = new EventEmitter<void>();
+
+  showConfirmation: boolean = false;
 
   best_friends: string[] = [];
 
@@ -45,13 +51,6 @@ export class AddBestFriendsComponent implements OnInit{
 
     this.follows = data['follows'] || [];
     this.best_friends = data['best_friends'] || [];
-
-    for (let u in this.best_friends) {
-      const user = await this.userService.getUserData(this.follows[u]);
-      if (!user) continue;
-
-      this.best_friends.push(user['uid']);
-    }
 
     for (let u in this.follows) {
       const user = await this.userService.getUserData(this.follows[u]);
@@ -95,5 +94,18 @@ export class AddBestFriendsComponent implements OnInit{
 
   isUserSelected(user: UserInfo): boolean {
     return this.selectedUsers.some(u => u.uid === user.uid);
+  }
+
+  onConfirm() {
+    this.saveBestFriends();
+    this.showConfirmation = false;
+  }
+
+  onCancel() {
+    this.showConfirmation = false;
+  }
+
+  showConfirm() {
+    this.showConfirmation = true;
   }
 }
