41801551336b555df373f42989c55c3b6c09d021
Omar <omar.ortega102@alu.ulpgc.es>
2025-05-06 16:13:30 +0100

    translation button

--- src/app/pages/configuration/user-configuration/user-configuration.component.ts
+++ src/app/pages/configuration/user-configuration/user-configuration.component.ts
@@ -1,26 +1,29 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import {LanguageService} from '../../../services/translate.service';
 import {TranslatePipe} from '@ngx-translate/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user-configuration',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    FormsModule
   ],
   templateUrl: './user-configuration.component.html',
+  standalone: true,
   styleUrl: './user-configuration.component.css'
 })
 export class UserConfigurationComponent {
   @Output() editProfile = new EventEmitter<void>();
+  selectedLang: string = 'es';
 
   constructor(private langService: LanguageService) {}
 
-  async ngOnInit() {
-    const lang = this.langService.currentLang;
-    await this.langService.changeLang(lang);
-  }
-
   onEditProfileClick(): void {
     this.editProfile.emit();
+  }
+
+  changeLanguage() {
+    this.langService.changeLang(this.selectedLang);
   }
 }
